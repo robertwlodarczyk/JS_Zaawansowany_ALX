22a92eb4044b5b3d68985f75973369037d026858
Robert_Wlodarczyk <robert.michal.wlodarczyk@gmail.com>
2022-03-19 10:38:50 +0100

    First commit

---
+++ cz1.js
@@ -0,0 +1,11 @@
+const products = [
+    { name: "Bananas", pricePerUnit: 1.49, quantity: 2 },
+    { name: "Bread", pricePerUnit: 3.29, quantity: 1 },
+    { name: "Chocolate", pricePerUnit: 5, quantity: 2 },
+  ];
+  
+  function sumItems() {
+    // Tu napisz swój kod
+  }
+  
+  sumItems(products) //powinno zwrócić 16.27
